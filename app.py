2d4580a6aec3103167880386b04834b6b8511c94
Ravi <ravirajking12@gmail.com>
2026-07-23 19:11:31 +0530

    dispalyed data recursively using for loop in jinja

--- app.py
+++ app.py
@@ -9,7 +9,12 @@ def home():
 
 @app.route('/market')
 def market():
-    return render_template('market.html', item_name="Summer Time Saga", price="$20")
+    items=[{'id':1, 'name':'Gta V', 'barcode':'78356291','price':'$80'},
+    {'id':2, 'name':'RDR 2', 'barcode':'85469209','price':'$89'},
+    {'id':3, 'name':'Summer Time Saga', 'barcode':'68915628','price':'$20'},
+    {'id':4, 'name':'My Femboy Roommate', 'barcode':'69696969','price':'$15'},
+    ]
+    return render_template('market.html',items=items)
 
 if __name__ == "__main__":
     app.run(debug=True)
