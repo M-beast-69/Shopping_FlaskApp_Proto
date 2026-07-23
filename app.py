e974703497a13cf7ef15a049e5df47707e699388
Ravi <ravirajking12@gmail.com>
2026-07-23 11:52:33 +0530

    added static and templates

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask as fk
+from flask import Flask, render_template
 
-app = fk(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "hello"
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
