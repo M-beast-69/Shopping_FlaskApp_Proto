5086c9cc81bcaf5e619bc74d8bb7c852eceabc67
Ravi <ravirajking12@gmail.com>
2026-07-23 17:47:23 +0530

    implimented bootstrap

--- app.py
+++ app.py
@@ -3,12 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def home():
     return render_template('home.html')
-
-@app.route('/<username>')
-def about(username):
-    return f'<h1>Hello {username}</h1>'
 
 if __name__ == "__main__":
     app.run(debug=True)
