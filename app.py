7c09116c2ed8775cdbe32c410335a536c630e490
Ravi <ravirajking12@gmail.com>
2026-07-23 16:56:09 +0530

    Tested dynamic routing

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/<username>')
+def about(username):
+    return f'<h1>Hello {username}</h1>'
+
 if __name__ == "__main__":
     app.run(debug=True)
